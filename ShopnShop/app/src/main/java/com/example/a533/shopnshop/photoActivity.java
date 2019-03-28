ff3b70a7b98fd147b63bf6f43311440fb42506ab
B533P-26\533 <eliot.eliot@hotmail.com>
2019-03-28 14:23:01 -0400

    commit photoss

--- ShopnShop/app/src/main/java/com/example/a533/shopnshop/photoActivity.java
+++ ShopnShop/app/src/main/java/com/example/a533/shopnshop/photoActivity.java
@@ -26,12 +26,22 @@ ImageView imageView;
         setContentView(R.layout.photo_activity);
 
         Button btncamera=findViewById(R.id.button);
+        Button btngallery=findViewById(R.id.button2);
         imageView =findViewById(R.id.imageVieww);
         btncamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-               startActivityForResult(intent,0);
+                Intent takePicture  =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+               startActivityForResult(takePicture ,0);
+
+            }
+        });
+        btngallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);
 
             }
         });
