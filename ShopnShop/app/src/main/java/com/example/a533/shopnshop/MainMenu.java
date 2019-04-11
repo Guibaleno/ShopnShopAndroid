47520820290f071224c2ad624384ed9e868a1944
B533P-27\533 <william.chiasson@hotmail.ca>
2019-04-11 14:23:35 -0400

    commit 2 menu incomplet

--- ShopnShop/app/src/main/java/com/example/a533/shopnshop/MainMenu.java
+++ ShopnShop/app/src/main/java/com/example/a533/shopnshop/MainMenu.java
@@ -23,36 +23,36 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
-        drawerLayout = findViewById(R.id.drawer_layout);
-
-        mDrawerLayout.addDrawerListener(
-                new DrawerLayout.DrawerListener() {
-                    @Override
-                    public void onDrawerSlide(View drawerView, float slideOffset) {
-                        // Respond when the drawer's position changes
-                    }
-
-                    @Override
-                    public void onDrawerOpened(View drawerView) {
-                        // Respond when the drawer is opened
-                    }
-
-                    @Override
-                    public void onDrawerClosed(View drawerView) {
-                        // Respond when the drawer is closed
-                    }
-
-                    @Override
-                    public void onDrawerStateChanged(int newState) {
-                        // Respond when the drawer motion state changes
-                    }
-                }
-        );
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        //ActionBar actionbar = getSupportActionBar();
+        //actionbar.setDisplayHomeAsUpEnabled(true);
+        //actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        //drawerLayout = findViewById(R.id.drawer_layout);
+//
+        //mDrawerLayout.addDrawerListener(
+        //        new DrawerLayout.DrawerListener() {
+        //            @Override
+        //            public void onDrawerSlide(View drawerView, float slideOffset) {
+        //                // Respond when the drawer's position changes
+        //            }
+//
+        //            @Override
+        //            public void onDrawerOpened(View drawerView) {
+        //                // Respond when the drawer is opened
+        //            }
+//
+        //            @Override
+        //            public void onDrawerClosed(View drawerView) {
+        //                // Respond when the drawer is closed
+        //            }
+//
+        //            @Override
+        //            public void onDrawerStateChanged(int newState) {
+        //                // Respond when the drawer motion state changes
+        //            }
+        //        }
+        //);
 
 
         btnOrder = findViewById(R.id.btnOrder);
