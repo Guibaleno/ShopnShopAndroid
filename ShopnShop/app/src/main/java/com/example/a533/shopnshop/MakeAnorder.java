281caddf9297cfad89f6fd8095def54f086ef7a1
B533P-26\533 <glambertgm@gmail.com>
2019-04-18 13:18:44 -0400

    sauvgarde

--- ShopnShop/app/src/main/java/com/example/a533/shopnshop/MakeAnorder.java
+++ ShopnShop/app/src/main/java/com/example/a533/shopnshop/MakeAnorder.java
@@ -1,5 +1,6 @@
 package com.example.a533.shopnshop;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,7 +16,7 @@ public class MakeAnorder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_makeanorder);
-
+        dbShop = new SQLite(this);
         recyclerItemsToOrder = findViewById(R.id.recyclerItemsToOrder);
         InsertDataIntoRecyclerView();
     }
@@ -26,6 +27,8 @@ public class MakeAnorder extends AppCompatActivity {
        LinearLayoutManager allo = new LinearLayoutManager(this);
        recyclerItemsToOrder.setLayoutManager(allo);
        List<String> itemsToSell = new ArrayList<String>();
+       Cursor items = dbShop.GetItemsToSell();
+       //items.getString("objectName");
        itemsToSell.add("test");
        itemsToSell.add("test2");
        RecyclerView.Adapter data = new recycleViewItem(itemsToSell);
